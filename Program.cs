09b1add94cd1100ae9794838c5ba634f343d5842
SamanthaBukowski <61812385+SamanthaBukowski@users.noreply.github.com>
2020-03-22 21:36:09 -0500

    HelloWorld!

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorldTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wash Your Hands!");
+            Console.WriteLine("Press key to exit.");
+            Console.ReadKey();
+        }
+        
+
+    }
+}
+
